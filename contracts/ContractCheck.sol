33ead9e6f9388c6ec80062123475a039d148f309
hugoroussel <hroussel96@gmail.com>
2022-09-18 01:45:00 +0200

    Comment contract code

--- contracts/ContractCheck.sol
+++ contracts/ContractCheck.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 contract ContractCheck {
 
+    // Certificate of the contract
     struct Certificate {
         bytes32 cid;
         string name;
@@ -25,7 +26,12 @@ contract ContractCheck {
     event Validated(bytes32 indexed certificateId, address indexed validator, address indexed contractAddress);
     event CertificateModified(bytes32 indexed certificateId, address indexed contractAddress, address indexed owner, string name);
 
-    // Public functions
+    /**
+     * newCertificate : creates a new certificate
+     * @param _name The name/description of the contract
+     * @param _contractAddress The contract address for which the certificate is created
+     * @param _chainId The chain id of the network where the contract is deployed
+    */
     function newCertificate(string memory _name, address _contractAddress, uint256 _chainId) public {
         bytes32 cid = _hash(_name, _contractAddress, msg.sender, _chainId);
         Certificate memory nc = Certificate(cid, _name, _contractAddress, msg.sender, _chainId, new address[](0), true, block.timestamp);
@@ -37,19 +43,31 @@ contract ContractCheck {
         emit NewCertificateCreated(cid, _contractAddress, msg.sender, _name);
     }
 
+    /**
+     * batchValidate : validate a list of certificates
+     * @param _certificateIds The certificate ids to validate
+    */
     function batchValidate(bytes32[] memory _certificateIds) public {
         for (uint i = 0; i < _certificateIds.length; i++) {
             _validate(_certificateIds[i]);
         }
     }
 
-
+    /**
+     * removeValidity : creates a new certificate
+     * @param _certificateId The certificate ids to remove validity
+    */
     function removeValidity(bytes32 _certificateId) public {
         require(certificateRegistry[_certificateId].owner == msg.sender, "Only the owner can remove the validity");
         certificateRegistry[_certificateId].valid = false;
     }
 
     // Internal functions
+
+    /**
+     * _validate : creates a new certificate
+     * @param _certificateId The certificate id to validate
+    */
     function _validate(bytes32 _certificateId) public {
         // TODO : do checks here
         require(certificateRegistry[_certificateId].owner != address(0), "Certificate does not exist");
@@ -60,26 +78,50 @@ contract ContractCheck {
         isValidator[_certificateId][msg.sender] = true;
         emit Validated(_certificateId, msg.sender, certificateRegistry[_certificateId].contractAddress);
     }
-    
+
     // Getters
+
+    /**
+     * getCertificateIds : returns all the certificate ids currently created
+     * @return The certificate ids
+    */
     function getCertificateIds() public view returns(bytes32[] memory) {
         return certificatesIds;
     }
 
+    /**
+     * getCertificatedIdsOfCertificatesValidatedByUser : returns all the certificate ids currently validated by a user
+     * @return The certificate ids
+    */
     function getCertificatedIdsOfCertificatesValidatedByUser(address _user) public view returns(bytes32[] memory) {
         return userValidatedCertificateIds[_user];
     }
 
+    /**
+     * getCertificatedIdsForContractAddress : returns all the certificate ids associated to a contract address
+     * @return The certificate ids
+    */
     function getCertificatedIdsForContractAddress(address _contract) public view returns(bytes32[] memory) {
         return contractAddressToCertificateIds[_contract];
     }
 
-    function getAllValidatorsOfContract(bytes32 _certificateId) public view returns(address[] memory) {
+    /**
+     * getAllValidatorsOfContract : returns all the validators of a certificate
+     * @param _certificateId The certificate id
+     * @return The addresses of the validators
+    */
+    function getAllValidatorsOfCertificate(bytes32 _certificateId) public view returns(address[] memory) {
         return certificateRegistry[_certificateId].validators;
     }
 
     // Helpers
-    // _hash creates a unique id for each certificate
+    /**
+     * _hash : creates a unique id for a certificate
+     * @param _name The name/description of the contract
+     * @param _contract The contract address for which the certificate is created
+     * @param _owner The creator of the certificate
+     * @param _chainId The chain id of the network where the contract is deployed
+    */
     function _hash(string memory _name, address _contract, address _owner, uint256 _chainId) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(_name, _contract, _owner, _chainId));
     }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,6 +7,14 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
-    }
-  }
+    },
+    optimisticEthereum:{
+      url: "https://opt-mainnet.g.alchemy.com/v2/"+process.env.ALCHEMY_KEY,
+      accounts: [process.env.KEY],
+      chainId: 10,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
+  },
 };
